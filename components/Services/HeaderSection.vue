e722bf6911ca4c93a86e636a639435fa72ca6c1e
parsa jiravand <parsajiravand@gmail.com>
2023-01-26 14:04:43 +0330

    setup service page and components

---
+++ components/Services/HeaderSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <section
+      class="page-header position-relative overflow-hidden ptb-120 bg-dark"
+      style="
+        background: url('assets/img/page-header-bg.svg') no-repeat bottom left;
+      "
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-12">
+            <h1 class="display-5 fw-bold">We are Development Experts</h1>
+            <p class="lead">
+              Seamlessly actualize client-based users after out-of-the-box
+              value. Globally embrace strategic data through frictionless
+              expertise.
+            </p>
+          </div>
+        </div>
+        <div
+          class="bg-circle rounded-circle circle-shape-3 position-absolute bg-dark-light right-5"
+        ></div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "ServiceHeaderSection",
+};
+</script>
---
+++ pages/services.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="main-wrapper">
+      <ServicesHeaderSection />
+      <ServicesFeautreSection />
+      <AboutUsTestimonial />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Services",
+};
+</script>
