4b738b0335fd52fe339e0c89f9d53e2ee21ce475
Muhammad <hi@trader4.net>
2023-02-24 00:07:00 +0000

    [ADD] Update layouts and links

--- pages/resources.vue
+++ pages/resources.vue
@@ -3,13 +3,13 @@
     <SharedPageHeaderSection
       title="Sample Title"
       desc="eamlessly actualize client-based users after out-of-the-box
-                    value. Globally embrace strategic data through frictionless
-                    expertise." />
-    AuthLoginPage
+                value. Globally embrace strategic data through frictionless
+                expertise." />
+    Career
   </div>
 </template>
 <script>
 export default {
-  name: "AuthLogin",
+  name: "Career",
 };
 </script>
