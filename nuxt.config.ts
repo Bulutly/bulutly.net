c5b46406b812ca8093cebb32ed32fb5dc3914110
parsajiravand <parsajiravand@gmail.com>
2023-02-26 12:49:37 +0330

    [FIX] import js files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  script: [
-    {
-      src: "/js/jquery.min.js",
+  app: {
+    head: {
+      script: [
+        {
+          src: "/js/jquery.min.js",
+        },
+        {
+          src: "/js/popper.min.js",
+        },
+        {
+          src: "/js/template-scripts.js",
+        },
+        {
+          src: "/js/flickity.pkgd.min.js",
+        },
+        {
+          src: "/js/parallax.min.js",
+        },
+        {
+          src: "/js/mailchamp.js",
+        },
+        {
+          src: "/js/bootstrap.offcanvas.min.js",
+        },
+        {
+          src: "/js/particles-code.js",
+        },
+        {
+          src: "/js/particles.js",
+        },
+        {
+          src: "/js/smoothscroll.js",
+        },
+        {
+          src: "/js/bootstrap.min.js",
+        },
+        {
+          src: "/owlcarousel/owl.carousel.min.js",
+        },
+      ],
     },
-    {
-      src: "/js/popper.min.js",
-    },
-    {
-      src: "/js/template-scripts.js",
-    },
-    {
-      src: "/js/flickity.pkgd.min.js",
-    },
-    {
-      src: "/js/parallax.min.js",
-    },
-    {
-      src: "/js/mailchamp.js",
-    },
-    {
-      src: "/js/bootstrap.offcanvas.min.js",
-    },
-    {
-      src: "/js/particles-code.js",
-    },
-    {
-      src: "/js/particles.js",
-    },
-    {
-      src: "/js/smoothscroll.js",
-    },
-    {
-      src: "/js/bootstrap.min.js",
-    },
-    {
-      src: "/owlcarousel/owl.carousel.min.js",
-    },
-  ],
+  },
   css: [
     "@/assets/css/bootstrap.min.css",
     "@/assets/icons-pack/fontawesome-5/css/all.css",
--- public/js/particles-code.js
+++ public/js/particles-code.js
@@ -1475,69 +1475,69 @@ function isInArray(value, array) {
 
 window.pJSDom = [];
 
-window.particlesJS = function(tag_id, params){
+// window.particlesJS = function(tag_id, params){
 
-  //console.log(params);
+//   //console.log(params);
 
-  /* no string id? so it's object params, and set the id with default id */
-  if(typeof(tag_id) != 'string'){
-    params = tag_id;
-    tag_id = 'particles-js';
-  }
+//   /* no string id? so it's object params, and set the id with default id */
+//   if(typeof(tag_id) != 'string'){
+//     params = tag_id;
+//     tag_id = 'particles-js';
+//   }
 
-  /* no id? set the id to default id */
-  if(!tag_id){
-    tag_id = 'particles-js';
-  }
+//   /* no id? set the id to default id */
+//   if(!tag_id){
+//     tag_id = 'particles-js';
+//   }
 
-  /* pJS elements */
-  var pJS_tag = document.getElementById(tag_id),
-      pJS_canvas_class = 'particles-js-canvas-el',
-      exist_canvas = pJS_tag.getElementsByClassName(pJS_canvas_class);
+//   /* pJS elements */
+//   var pJS_tag = document.getElementById(tag_id),
+//       pJS_canvas_class = 'particles-js-canvas-el',
+//       exist_canvas = pJS_tag.getElementsByClassName(pJS_canvas_class);
 
-  /* remove canvas if exists into the pJS target tag */
-  if(exist_canvas.length){
-    while(exist_canvas.length > 0){
-      pJS_tag.removeChild(exist_canvas[0]);
-    }
-  }
+//   /* remove canvas if exists into the pJS target tag */
+//   if(exist_canvas.length){
+//     while(exist_canvas.length > 0){
+//       pJS_tag.removeChild(exist_canvas[0]);
+//     }
+//   }
 
-  /* create canvas element */
-  var canvas_el = document.createElement('canvas');
-  canvas_el.className = pJS_canvas_class;
+//   /* create canvas element */
+//   var canvas_el = document.createElement('canvas');
+//   canvas_el.className = pJS_canvas_class;
 
-  /* set size canvas */
-  canvas_el.style.width = "100%";
-  canvas_el.style.height = "100%";
+//   /* set size canvas */
+//   canvas_el.style.width = "100%";
+//   canvas_el.style.height = "100%";
 
-  /* append canvas */
-  var canvas = document.getElementById(tag_id).appendChild(canvas_el);
+//   /* append canvas */
+//   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
 
-  /* launch particle.js */
-  if(canvas != null){
-    pJSDom.push(new pJS(tag_id, params));
-  }
+//   /* launch particle.js */
+//   if(canvas != null){
+//     pJSDom.push(new pJS(tag_id, params));
+//   }
 
-};
+// };
 
-window.particlesJS.load = function(tag_id, path_config_json, callback){
+// window.particlesJS.load = function(tag_id, path_config_json, callback){
 
-  /* load json config */
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', path_config_json);
-  xhr.onreadystatechange = function (data) {
-    if(xhr.readyState == 4){
-      if(xhr.status == 200){
-        var params = JSON.parse(data.currentTarget.response);
-        window.particlesJS(tag_id, params);
-        if(callback) callback();
-      }else{
-        console.log('Error pJS - XMLHttpRequest status: '+xhr.status);
-        console.log('Error pJS - File config not found');
-      }
-    }
-  };
-  xhr.send();
+//   /* load json config */
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', path_config_json);
+//   xhr.onreadystatechange = function (data) {
+//     if(xhr.readyState == 4){
+//       if(xhr.status == 200){
+//         var params = JSON.parse(data.currentTarget.response);
+//         window.particlesJS(tag_id, params);
+//         if(callback) callback();
+//       }else{
+//         console.log('Error pJS - XMLHttpRequest status: '+xhr.status);
+//         console.log('Error pJS - File config not found');
+//       }
+//     }
+//   };
+//   xhr.send();
 
-};
+// };
 
