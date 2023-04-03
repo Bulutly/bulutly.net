682bb23abf19193dd2c77672ca8aa0cc9208e93b
parsajiravand <parsajiravand@gmail.com>
2023-04-03 22:12:16 +0330

    setup vue gtag | runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,7 @@ export default defineNuxtConfig({
         { name: "twitter:card", content: "summary_large_image" },
         {
           name: "twitter:title",
-          content:
-            "Trader4 - Just A Trader, Level Up Your Trading",
+          content: "Trader4 - Just A Trader, Level Up Your Trading",
         },
         {
           name: "twitter:description",
@@ -66,4 +65,10 @@ export default defineNuxtConfig({
   plugins: [
     // only on client side
   ],
+  runtimeConfig: {
+    // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-runtime-config
+    public: {
+      googleAnalyticsId:''
+    },
+  },
 });
