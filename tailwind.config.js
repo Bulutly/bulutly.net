c6e73f0aa1bed4ac675c6d13cfb0c666ed035aea
parsajiravand <parsajiravand@gmail.com>
2023-04-15 10:52:45 +0330

    setup new trader4 app

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    // other files...
+    "./node_modules/flowbite.{js,ts}"
+  ],
+  darkMode: "class",
+  variants: {
+    backgroundColor: [
+      "dark",
+      "dark-hover",
+      "dark-group-hover",
+      "dark-even",
+      "dark-odd",
+    ],
+    borderColor: ["dark", "dark-focus", "dark-focus-within"],
+    textColor: ["dark", "dark-hover", "dark-active"],
+  },
+  plugins: [
+    require('flowbite')
+  ],
+};
