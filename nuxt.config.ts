1ca8f1e565488769e1f80b2c95cbeecbb64e2734
briofy-mammad <m.ranjbar@briofy.net>
2023-04-18 16:07:35 +0330

    add auth composable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [
-    "github:traderfour/base-trader4-layers", // Extend from a git repository
+    "../../trader4/base-trader4-layers", // Extend from a git repository
   ],
 });
